a4dfa9c34a57111b1cc49e42d28bf42e97f1b435
Diana Laura <101437029+DianaCapellino@users.noreply.github.com>
2025-06-19 15:31:51 -0300

    Update settings.py

--- sayhueque/settings.py
+++ sayhueque/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q$*#taobnf_ih6vno&mu!)%t33whv44w-k*2y_hnj1+z)0)&6y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
