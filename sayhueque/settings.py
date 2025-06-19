26f3d06ca5f14c32f3db5d37b1c180af0d11330d
Diana Laura <101437029+DianaCapellino@users.noreply.github.com>
2025-06-19 14:10:03 -0300

    Update settings.py

--- sayhueque/settings.py
+++ sayhueque/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q$*#taobnf_ih6vno&mu!)%t33whv44w-k*2y_hnj1+z)0)&6y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
