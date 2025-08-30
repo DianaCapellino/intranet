e7354f2e3c4db753fbf2f64931c2a92589645858
DianaCapellino <diana.capellino@gmail.com>
2025-08-30 11:34:09 -0300

    Arranging margins at digital ocean

--- sayhueque/settings.py
+++ sayhueque/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['intranet-app-2x5m6.ondigitalocean.app/']
+ALLOWED_HOSTS = ['intranet-app-2x5m6.ondigitalocean.app']
 
 
 # Application definition
